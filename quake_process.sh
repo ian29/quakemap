fe8b9d50bba4f015e7cda8232e6947b1f42048bf
ian29 <ian.villeda@gmail.com>
2012-09-30 20:45:46 -0400

    lots of small changes, fixes #4

--- quake_process.sh
+++ quake_process.sh
@@ -2,20 +2,20 @@
 set -e -u
 
 # GOALS:
-# this script attempts to do the following, making it easier to make fast maps 
+# this script attempts to do the following, making it easier to make fast maps
 # 	out of the USGS shakemap product: http://earthquake.usgs.gov/research/shakemap/
 #
 # 1. eat an .xyz file of earthquake data from USGS
 # 2. clean + import that file to a postgres table
-# 3. run some basic sql to get min,max and avg out of a user-specified column 
+# 3. run some basic sql to get min,max and avg out of a user-specified column
 # 4. use those values in a color ramp
 # 5. export to geotiff, and then recolor that tif with the color ramp
 #
-# TODO: 
+# TODO:
 # - correctly assign variable values for pg_values
 # - gdaldem color-relief before export
 # - && in conditional "do all the things"
-# - use sed to remove first line of stupid grid.xyz
+# - use `min + some %` rather than `1` as for some ranges, 1 is not negligible 
 # - import specified input file
 # 	- test psql + COPY
 # 	- test pg_values with table
@@ -27,9 +27,9 @@ usage: $0 [OPTION]
 -i              Import all XYZ files
 -e              Export a GeoTIFF
 -c column       specify which column you want to make a map of
--t table       	name the table 
+-t table       	name the table
 -o FILE         Path of the output file. Defaults to the name of the postgres table. 
--h				Display this help screen
+-h              Display this help screen
 
 Example:
     $0 -c psa03 -t costa_rica grid.xyz costa-rica-pga.tif
@@ -48,8 +48,9 @@ pg_setup() {
 import_xyz() {
     echo "Importing $1..."
     tempm=$(mktemp)
-	sed '1d' $inputfile > $tempm
+    sed '1d' $inputfile > $tempm
     $pgcmd -c "copy $t (x,y,) from stdin with delimiter ' ' csv" < $1
+    rm $tempm
 }
 
 pg_cleanup() {
@@ -59,13 +60,14 @@ pg_cleanup() {
 
 pg_values() {
 	echo "Calculating…"
+	# is this really how you assign variables?
 	min='$pgcmd -c "select min($c) from $table"'
 	max='$pgcmd -c "select max($c) from $table"'
 	avg='$pgcmd -c "select avg($c) from $table"'
 }
 
-write_ramp()
-	echo "Create color ramp"
+write_ramp() {
+	echo "Creating color ramp"
 	min1=$[min + 1]
 	echo "$min	255	255	255" > ${table}-ramp.txt
 	echo "$min1	252	255	238" >> ${table}-ramp.txt
@@ -80,13 +82,22 @@ export() {
     tempfile=$(mktemp)
 
     # Export as XYZ from the PostgreSQL DB to a temporary file
-    $pgcmd -c "copy (select x, y, $c) to stdout with csv delimiter ' ';" > $tempfile
+    $pgcmd -c "copy (select x, y, $c) to stdout with csv delimiter ' ';" > $temp1
 
     # Convert the tempfile to a GeoTIFF
-    gdal_translate -co compress=lzw -a_srs EPSG:4326 $tempfile $outfile
+    gdal_translate -co compress=lzw -a_srs EPSG:4326 $temp1 $temp2
 
     # Clean up
-    rm $tempfile
+    rm $temp1
+}
+
+mapify() {
+	# Colorize
+	gdaldem color-relief ${table}-ramp.txt $temp2 $temp3 
+	
+	# Reproject
+	gdalwarp -s_srs EPSG:4326 -t_srs EPSG:900913 $temp3 $outfile
+
 }
 
 column=pga
@@ -117,7 +128,8 @@ while getopts “hief:t:o:” OPTION; do
 done
 
 # Do all the things!
-if [ $opt_import = 0 ]; then
+#	but is the the right way to do 'and'??
+if [ $opt_import = 0 ] && [ $opt_export = 0 ]; then
     pg_setup
     import_xyz $f
     pg_cleanup
